92a1f4eb9089c04699ccc7fc6a55fc3e61b771bd
rdarshan927 <rdarshan927@gmail.com>
2024-09-02 18:43:24 +0530

    dsa lab test 1 - quiz 1 answer added

---
+++ y2s2dsatest1/quiz1/QueueStateException.java
@@ -0,0 +1,9 @@
+package quiz1;
+
+public class QueueStateException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public QueueStateException(String message) {
+		super(message);
+	}
+}
---
+++ y2s2dsatest1/quiz1/StackStateException.java
@@ -0,0 +1,9 @@
+package quiz1;
+
+public class StackStateException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public StackStateException(String message) {
+		super(message);
+	}
+}
